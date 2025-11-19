590aa4c79f5002efe3ce7968b3d21362c6846c1f
jun <pickjog@gmail.com>
2025-11-19 14:18:37 +0900

    feat(application): 상품 서비스 관련 생성 단일 허브 재고 생성으로 수정

--- src/main/java/com/early_express/product_service/domain/product/domain/messaging/ProductEventPublisher.java
+++ src/main/java/com/early_express/product_service/domain/product/domain/messaging/ProductEventPublisher.java
@@ -12,10 +12,20 @@ public interface ProductEventPublisher {
     /**
      * 상품 생성 이벤트 발행
      */
-    void publishProductCreated(Product product);
+    void publishProductCreated(Product product, String hubId);
 
     /**
      * 상품 상태 변경 이벤트 발행
      */
     void publishProductStatusChanged(String productId, ProductStatus oldStatus, ProductStatus newStatus);
+
+    /**
+     * 상품 삭제(단종) 이벤트 발행
+     */
+    void publishProductDeleted(String productId, String sellerId);
+
+    /**
+     * 상품 정보 수정 이벤트 발행
+     */
+    void publishProductUpdated(Product product);
 }
